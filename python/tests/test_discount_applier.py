2115a28f6861458d7de724040b4b81aac19e6328
Taink <tainkk@protonmail.com>
2024-02-09 14:22:33 +0100

    Make discount_applier test less general

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -2,17 +2,18 @@ from discount_applier import DiscountApplier
 
 def test_apply_v1():
     discount = 5
-    fake_users = ['user1', 'user2', 'user3']
-    notified_users = list()
+    notified_users = 0
+    fake_users = ['user1', 'user2', 'user3']    
 
     class FakeNotifier:
         def notify(self, user, message):
-            notified_users.append(user)
+            nonlocal notified_users
+            notified_users += 1
             
     discount_applier = DiscountApplier(FakeNotifier())
     discount_applier.apply_v1(discount, fake_users)
     
-    assert fake_users == notified_users
+    assert notified_users == len(fake_users)
 
 
 def test_apply_v2():
