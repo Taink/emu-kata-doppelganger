7f19f17fa18155bd4a782a973741066b33d623e9
Taink <tainkk@protonmail.com>
2024-02-09 15:01:11 +0100

    Move FakeNotifier outside of tests for discount_applier
    Co-authored-by: Sébastien CUVELLIER <Saibe1111@users.noreply.github.com>
    Co-authored-by: Al1ch <Al1ch@users.noreply.github.com>

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,30 +1,34 @@
 from discount_applier import DiscountApplier
 
+class FakeNotifier:
+    def __init__(self):
+        self.notified_users: list[str] = list()
+
+    def notify(self, user, message):
+        self.notified_users.append(user)
+        
+    def get_notified_users(self):
+        return self.notified_users
+
 def test_apply_v1():
     discount = 5
-    notified_users = 0
-    fake_users = ['user1', 'user2', 'user3']    
+    fake_users = ['user1', 'user2', 'user3']
+    notifier = FakeNotifier()
 
-    class FakeNotifier:
-        def notify(self, user, message):
-            nonlocal notified_users
-            notified_users += 1
-            
-    discount_applier = DiscountApplier(FakeNotifier())
+    discount_applier = DiscountApplier(notifier)
     discount_applier.apply_v1(discount, fake_users)
+    notified_users = notifier.get_notified_users()
     
-    assert notified_users == len(fake_users)
+    assert len(notified_users) == len(fake_users)
 
 
 def test_apply_v2():
-    expected_users = ['user1', 'user2', 'user3']    
-    notified_users: list[str] = list()
+    discount = 5
+    fake_users = ['user1', 'user2', 'user3']
+    notifier = FakeNotifier()
 
-    class FakeNotifier:
-        def notify(self, user, message):
-            notified_users.append(user)
+    discount_applier = DiscountApplier(notifier)
+    discount_applier.apply_v1(discount, fake_users)
+    notified_users = notifier.get_notified_users()
     
-    discount_applier = DiscountApplier(FakeNotifier())
-    discount_applier.apply_v2(5, expected_users)
-
-    assert notified_users == expected_users
+    assert notified_users == fake_users
