83ac130ad96a1972cad0ad2c30f96b265fff7771
Taink <tainkk@protonmail.com>
2024-02-09 14:11:51 +0100

    Fix todos for discount_applier
    Co-authored-by: Sébastien CUVELLIER <Saibe1111@users.noreply.github.com>
    Co-authored-by: Al1ch <Al1ch@users.noreply.github.com>

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,10 +1,29 @@
+from discount_applier import DiscountApplier
+
 def test_apply_v1():
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v1
-    pass
+    discount = 5
+    fake_users = ['user1', 'user2', 'user3']
+    notified_users = list()
+
+    class FakeNotifier:
+        def notify(self, user, message):
+            notified_users.append(user)
+            
+    discount_applier = DiscountApplier(FakeNotifier())
+    discount_applier.apply_v1(discount, fake_users)
+    
+    assert fake_users == notified_users
 
 
 def test_apply_v2():
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v2
-    pass
+    expected_users = ['user1', 'user2', 'user3']    
+    notified_users: list[str] = list()
+
+    class FakeNotifier:
+        def notify(self, user, message):
+            notified_users.append(user)
+    
+    discount_applier = DiscountApplier(FakeNotifier())
+    discount_applier.apply_v2(5, expected_users)
+
+    assert notified_users == expected_users
